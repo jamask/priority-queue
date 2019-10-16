df6697670bb546cc81162fe523892ef3107f99e9
Askhat <jam.askhat@gmail.com>
2019-10-17 01:33:29 +0600

    feat: solve some tests

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,11 +2,13 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		let tmp = new Node(data, priority);
+		this.insertNode(tmp);
 	}
 
 	pop() {
@@ -30,16 +32,43 @@ class MaxHeap {
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
+
 	insertNode(node) {
-		
+		if (!this.root) {
+			this.root = node;
+			this.parentNodes.push(node);
+		} else {
+			let parn = this.parentNodes[0];
+			parn.appendChild(node);
+			this.parentNodes.push(node);
+
+			if (parn.right != null){
+        this.parentNodes.shift();
+      }
+		}
+
 	}
 
 	shiftNodeUp(node) {
 		
-	}
+			// let tmp = this.parentNodes.pop();
+			if (node.parent && node.priority>node.parent.priority ) {
+				
+				node.swapWithParent();
+				this.shiftNodeUp(node);
+			} else {
+				return;
+			}
+			// this.parentNodes.unshift(tmp);
+		
+		
+
+		
+		}
 
 	shiftNodeDown(node) {
 		
--- src/node.js
+++ src/node.js
@@ -1,22 +1,72 @@
 class Node {
 	constructor(data, priority) {
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 
 	}
 
 	appendChild(node) {
-
+		if (!this.left) {
+			this.left = node;
+			node.parent = this;
+		} else if (!this.right) {
+			this.right = node;
+			node.parent = this;
+		}
 	}
 
 	removeChild(node) {
-
+		if (this.left == node) {
+			this.left = null;
+			node.parent = null;
+		} else if (this.right == node) {
+			this.right = null;
+			node.parent = null;
+		} else {
+			throw Error();
+		}
 	}
 
 	remove() {
-
+		if (this.parent) {
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
+		if (this.parent) {
+			let ll = this.left;
+			let rr = this.right;
+			this.left = null;
+			this.right = null;
+			let parn = this.parent;
+			parn.removeChild(this);
+			
+			if (parn.parent) {
+				let grandparn = parn.parent;
+				parn.parent.removeChild(parn);
+				grandparn.appendChild(this);
+			} else {
+				this.parent = null;
+			}
+			if (parn.left) {
+				this.appendChild(parn.left);
+				parn.left = null;
+			}
+			let parright = null;
+			if (parn.right) {
+				parright = parn.right;
+				parn.right = null;
+			}
+			if (ll) parn.appendChild(ll);
+			if (rr) parn.appendChild(rr);
+			this.appendChild(parn);
+			if (parright) this.appendChild(parright);
 		
+		}
 	}
 }
 
